afd060dbf5f147ecc03f93d2b20efa955b67ae31
Skrytt <matt.kemp.1992@gmail.com>
2018-06-09 18:06:06 +0100

    Clean up main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3.5
+""" Discord chat bot.
+"""
 
 import logging
 import sys
@@ -14,72 +16,96 @@ import stream_notification_utils
 import twitter_client
 import twitter_scheduler
 
+
 if __name__ != '__main__':
     print('Importing of this module is unsupported')
     sys.exit(1)
 
-logger = logging.getLogger(__name__)
 
-client = discord.Client()
+LOGGER = logging.getLogger(__name__)
+CLIENT = discord.Client()
 
-# Construct the config object and use it to initialize logging before we
-# actually load the real config
-config = config_utils.Config(logger, client)
-misc_utils.initializeLogger(logger, config)
+# Construct the config object first.
+# Use the config to initialize the logger before we load the real config
+CONFIG = config_utils.Config(LOGGER, CLIENT)
+misc_utils.initializeLogger(LOGGER, CONFIG)
 
-if not config.load():
-    logger.info('Cannot proceed without configuration - exiting.')
+if not CONFIG.load():
+    LOGGER.info('Cannot proceed without configuration - exiting.')
     sys.exit(1)
 
-twitter_client.initialize(config, logger)
+twitter_client.initialize(CONFIG, LOGGER)
 
 # Now set the configured log level
-misc_utils.setLogLevel(logger, config)
+misc_utils.setLogLevel(LOGGER, CONFIG)
 
-database = database_utils.Database(config)
-server_data_map = server_utils.ServerDataMap(logger, database)
+# Redis backend for persistent storage
+DATABASE = database_utils.Database(CONFIG)
+
+# Mapping of data specific to particular Discord servers
+SERVER_DATA_MAP = server_utils.ServerDataMap(LOGGER, DATABASE)
+
+# Dispatcher: knows how to route commands to the objects which will handle them
+DISPATCHER = dispatcher_utils.Dispatcher(
+    LOGGER, CONFIG, CLIENT, SERVER_DATA_MAP, DATABASE)
+
+#############################
+# Non-command functionality #
+#############################
+
+# Twitter scheduler: periodically tries to send Tweets to channels
 twitter_scheduler.initialize(
-        config, logger, server_data_map, client)
-dispatcher = dispatcher_utils.Dispatcher(
-        logger, config, client, server_data_map, database)
-stream_notifications = stream_notification_utils.StreamNotifications(
-        logger, config, client, server_data_map)
+    CONFIG, LOGGER, SERVER_DATA_MAP, CLIENT)
 
-@client.event
+# Stream notifications: reacts to Twitch streams starting and notifies Discord users
+STREAM_NOTIFICATIONS = stream_notification_utils.StreamNotifications(
+    LOGGER, CONFIG, CLIENT, SERVER_DATA_MAP)
+
+
+@CLIENT.event
 async def on_ready():
-    ''' Called when bot is logged into Discord.
+    """ Called when bot is logged into Discord.
         This doesn't necessarily mean the bot is a member of a server yet.
-    '''
-    logger.info('Logged in as user with name %r and ID %r', client.user.name, client.user.id)
-    config.load()
+    """
+    LOGGER.info('Logged in as user with name %r and ID %r', CLIENT.user.name, CLIENT.user.id)
+    CONFIG.load()
 
     # Schedule Twitter stuffs
-    for server in client.servers:
-        logger.debug('Joined server %r', server.name)
+    for server in CLIENT.servers:
+        LOGGER.debug('Joined server %r', server.name)
         twitter_scheduler.scheduler.start(server)
 
-@client.event
+
+@CLIENT.event
 async def on_message(message):
-    ''' Called whenever a message is received from Discord.
-    '''
+    """ Called whenever a message is received from Discord. """
+
     # Bot loopback protection
-    if message.author == config.getClientId():
+    if message.author == CONFIG.getClientId():
         return
+
     # Dispatch command
-    await dispatcher.dispatch(message)
+    await DISPATCHER.dispatch(message)
 
-@client.event
+
+@CLIENT.event
 async def on_member_update(member_before, member_after):
-    ''' Called when a Member updates their profile.
-    '''
-    await stream_notifications.onMemberUpdate(member_before, member_after)
+    """ Called when a Member updates their profile. """
+    await STREAM_NOTIFICATIONS.onMemberUpdate(member_before, member_after)
 
-# Initialization complete
-misc_utils.printInviteLink(config)
+
+# We are set up and the Discord client hooks are defined.
+# Now run the bot..:
+
+misc_utils.printInviteLink(CONFIG)
+
 try:
-    client.run(config.getToken())
+    CLIENT.run(CONFIG.getToken())
+
 except Exception as exc:
-    logger.error('Handled top level exception: %r', exc)
-    misc_utils.logTraceback(logger)
+    LOGGER.error('Handled top level exception: %r', exc)
+    misc_utils.logTraceback(LOGGER)
+    raise
+
 finally:
-    client.close()
+    CLIENT.close()
