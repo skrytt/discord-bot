09dfc7270430f68cc13f9f71410b4aeb4c940223
skrytt <matt.kemp.1992@gmail.com>
2018-06-17 14:16:53 +0100

    docker-compose for service

--- discord-bot.sh
+++ discord-bot.sh
@@ -6,75 +6,24 @@ if ! [ "$(id -u)" = "0" ]; then
   exit 1
 fi
 
-DISCORD_BOT_CONTAINER_NAME=discord-bot
+# Run from the directory with the compose file
+if ! [ -e docker-compose.yml ]; then
+  echo "Run from the directory containing the bot docker-compose.yml file"
+  exit 1
+fi
+
 DISCORD_BOT_IMAGE_NAME=discord-bot:latest
 
 build_discord_bot_image () {
-  if ! docker build . -t "$DISCORD_BOT_IMAGE_NAME"
-  then
-    echo "Failed to build '$DISCORD_BOT_IMAGE_NAME' image"
-    exit 1
-  fi
-
-  echo "Built '$DISCORD_BOT_IMAGE_NAME' image"
+  docker build . -t "$DISCORD_BOT_IMAGE_NAME"
 }
 
-kill_discord_bot () {
-  RUNNING_CONTAINER_ID="$(docker ps -qf name=^/$DISCORD_BOT_CONTAINER_NAME$)"
-  if ! [ "$RUNNING_CONTAINER_ID" ]
-  then
-    echo "Container '$DISCORD_BOT_CONTAINER_NAME' is not running!"
-    exit 1
-  fi
-
-  if ! docker kill "$RUNNING_CONTAINER_ID"
-  then
-    echo "Failed to kill container '$DISCORD_BOT_CONTAINER_NAME'"
-    exit 1
-  fi
-
-  echo "Killed container '$DISCORD_BOT_CONTAINER_NAME'"
+compose_down () {
+  docker-compose down
 }
 
-rm_discord_bot () {
-  CONTAINER_IDS="$(docker ps -aqf name=^/$DISCORD_BOT_CONTAINER_NAME$)"
-  if ! [ "$CONTAINER_IDS" ] ; then
-    echo "Container '$DISCORD_BOT_CONTAINER_NAME' does not exist!"
-    exit 1
-  fi
-
-  if ! docker rm $CONTAINER_IDS
-  then
-    echo "Failed to remove container(s)"
-    exit 1
-  fi
-
-  echo "Removed containers matching name '$DISCORD_BOT_CONTAINER_NAME'"
-}
-
-run_discord_bot () {
-  RUNNING_CONTAINER_ID="$(docker ps -qf name=^/$DISCORD_BOT_CONTAINER_NAME$)"
-  if [ "$RUNNING_CONTAINER_ID" ] ; then
-    echo "Container '$DISCORD_BOT_CONTAINER_NAME' is already running!"
-    exit 1
-  fi
-
-  # The following prerequisites must have been satisfied for this to work:
-  # 1. A docker volume "discord-bot-config" was created and contains the config.json config file
-  # 2. A redis instance is accessible at "discord-bot-redis:6379"
-
-  if ! docker run -d \
-      --name "$DISCORD_BOT_CONTAINER_NAME" \
-      -v discord-bot-config:/opt/discord-bot/config \
-      --network discord-bot-network \
-      --restart=always \
-      discord-bot
-  then
-    echo "Failed to start container '$DISCORD_BOT_CONTAINER_NAME'"
-    exit 1
-  fi
-
-  echo "Started container '$DISCORD_BOT_CONTAINER_NAME'"
+compose_up () {
+  docker-compose up -d
 }
 
 ACTION=$1
@@ -82,16 +31,17 @@ case $ACTION in
   build)
     build_discord_bot_image
     ;;
-  kill)
-    kill_discord_bot
+  down)
+    compose_down
     ;;
-  rm)
-    rm_discord_bot
+  up)
+    compose_up
     ;;
-  run)
-    run_discord_bot
+  restart)
+    compose_down
+    compose_up
     ;;
   :)
-    echo "Usage: $0 <stop|start>"
+    echo "Usage: $0 <build|down|up|restart>"
     ;;
 esac
